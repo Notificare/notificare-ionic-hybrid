db77739d8dc25f35ec20e390e71a8db5fec28a37
Helder Pinhal <helder@notifica.re>
2020-06-08 12:13:04 +0100

    Add placeholder to track events

--- src/pages/analytics/index.tsx
+++ src/pages/analytics/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from 'react';
-import { IonImg, IonInput, IonItem, IonLabel, IonSpinner } from '@ionic/react';
+import { IonImg, IonInput, IonItem, IonLabel, IonSpinner, IonButton } from '@ionic/react';
 import HeaderImage from '../../assets/images/profits.png';
 import { Center } from '../../components/center';
 import { PageContainer } from '../../components/page-container';
@@ -9,6 +9,11 @@ export const Analytics: FC = () => {
 
   const [event, setEvent] = useState('');
 
+  const onTrackEvent = async () => {
+    try {
+    } catch (e) {}
+  };
+
   return (
     <PageContainer title="Analytics">
       {loading && (
@@ -17,7 +22,7 @@ export const Analytics: FC = () => {
         </Center>
       )}
 
-      <IonItem>
+      <IonItem lines="none">
         <IonImg src={HeaderImage} className="header-image ion-padding" />
       </IonItem>
 
@@ -25,6 +30,10 @@ export const Analytics: FC = () => {
         <IonLabel position="floating">Event name</IonLabel>
         <IonInput value={event} onIonChange={(e) => setEvent(e.detail.value!)} />
       </IonItem>
+
+      <IonButton expand="block" className="ion-padding" onClick={onTrackEvent}>
+        Track event
+      </IonButton>
     </PageContainer>
   );
 };
