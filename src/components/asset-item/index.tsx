a54c0e74ec6ca9dd78cc52541165afc5a9459a00
Helder Pinhal <helder@notifica.re>
2020-06-10 15:48:22 +0100

    Fetch storage from Notificare

--- src/components/asset-item/index.tsx
+++ src/components/asset-item/index.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from 'react';
+import { NotificareAsset } from '@ionic-native/notificare';
 import { IonCard, IonCardHeader, IonCardSubtitle, IonImg } from '@ionic/react';
-import { NotificareAsset } from '../../lib/notificare/models';
+import AssetCss from '../../assets/images/asset_css.png';
+import AssetHtml from '../../assets/images/asset_html.png';
+import AssetJavascript from '../../assets/images/asset_js.png';
+import AssetJson from '../../assets/images/asset_json.png';
+import AssetPdf from '../../assets/images/asset_pdf.png';
+import AssetSound from '../../assets/images/asset_sound.png';
+import AssetText from '../../assets/images/asset_text.png';
+import AssetVideo from '../../assets/images/asset_video.png';
 import './index.scss';
 
-export const AssetItem: FC<AssetItemProps> = ({ asset }) => {
+export const AssetItem: FC<AssetItemProps> = ({ asset, onClick }) => {
+  const assetImage = computeAssetImage(asset);
+
   return (
-    <IonCard>
-      {asset.assetUrl && <IonImg src={asset.assetUrl} className="asset-image" />}
+    <IonCard onClick={onClick}>
+      {assetImage && <IonImg src={assetImage} className="asset-image" />}
       <IonCardHeader>
         <IonCardSubtitle>{asset.assetTitle}</IonCardSubtitle>
       </IonCardHeader>
@@ -16,4 +26,32 @@ export const AssetItem: FC<AssetItemProps> = ({ asset }) => {
 
 interface AssetItemProps {
   asset: NotificareAsset;
+  onClick: () => void;
+}
+
+function computeAssetImage(asset: NotificareAsset): string | undefined {
+  const contentType = asset.assetMetaData?.contentType;
+
+  switch (contentType) {
+    case 'image/jpeg':
+    case 'image/gif':
+    case 'image/png':
+      return asset.assetUrl;
+    case 'video/mp4':
+      return AssetVideo;
+    case 'application/pdf':
+      return AssetPdf;
+    case 'application/json':
+      return AssetJson;
+    case 'text/javascript':
+      return AssetJavascript;
+    case 'text/css':
+      return AssetCss;
+    case 'text/html':
+      return AssetHtml;
+    case 'audio/mp3':
+      return AssetSound;
+    default:
+      return AssetText;
+  }
 }
--- src/pages/storage/index.tsx
+++ src/pages/storage/index.tsx
@@ -1,53 +1,36 @@
 import React, { FC, FormEvent, useState } from 'react';
+import { Notificare, NotificareAsset } from '@ionic-native/notificare';
 import { IonCol, IonGrid, IonRow, IonSearchbar, IonSpinner } from '@ionic/react';
 import { AssetItem } from '../../components/asset-item';
 import { Center } from '../../components/center';
 import { PageContainer } from '../../components/page-container';
 import { useNetworkRequest } from '../../lib/network-request';
-import { NotificareAsset } from '../../lib/notificare/models';
 
 export const Storage: FC = () => {
-  const [search, setSearch] = useState('');
-  const [request, requestActions] = useNetworkRequest(
-    () =>
-      new Promise<NotificareAsset[]>((resolve, reject) =>
-        resolve([
-          {
-            assetTitle: 'asdasd',
-            assetUrl: 'https://placekitten.com/g/200/300',
-          },
-          {
-            assetTitle: 'asdasd',
-            assetUrl: 'https://placekitten.com/g/200/300',
-          },
-          {
-            assetTitle: 'asdasd',
-            assetUrl: 'https://placekitten.com/g/200/300',
-          },
-          {
-            assetTitle: 'asdasd',
-            assetUrl: 'https://placekitten.com/g/200/300',
-          },
-          {
-            assetTitle: 'asdasd',
-            assetUrl: 'https://placekitten.com/g/200/300',
-          },
-          {
-            assetTitle: 'asdasd',
-            assetUrl: 'https://placekitten.com/g/200/300',
-          },
-          {
-            assetTitle: 'asdasd',
-            assetUrl: 'https://placekitten.com/g/200/300',
-          },
-        ]),
-      ),
-  );
+  const [search, setSearch] = useState('LANDSCAPES');
+  const [request, requestActions] = useNetworkRequest(() => Notificare.fetchAssets(search));
 
   const onSearchSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     requestActions.start().catch(() => {});
+  };
+
+  const onShowAsset = async (asset: NotificareAsset) => {
+    const contentType = asset.assetMetaData?.contentType;
+    switch (contentType) {
+      case 'image/jpeg':
+      case 'image/gif':
+      case 'image/png':
+      case 'video/mp4':
+      case 'application/pdf':
+      case 'text/html':
+      case 'audio/mp3':
+        if (asset.assetUrl != null) {
+          window.open(asset.assetUrl);
+        }
+        break;
+    }
   };
 
   return (
@@ -77,12 +60,14 @@ export const Storage: FC = () => {
           <IonRow>
             {request.result.map((asset, index) => (
               <IonCol key={index} size="6">
-                <AssetItem asset={asset} />
+                <AssetItem asset={asset} onClick={() => onShowAsset(asset)} />
               </IonCol>
             ))}
           </IonRow>
         </IonGrid>
       )}
+
+      {request.status === 'failed' && <Center>Asset group not found.</Center>}
     </PageContainer>
   );
 };
