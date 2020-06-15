8f41a4dd36baa3822fc06981637acb6cca9d38ca
Helder Pinhal <helder@notifica.re>
2020-06-15 09:35:38 +0100

    Add iframe to home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,32 +1,11 @@
 import React, { FC } from 'react';
-import { IonItem, IonLabel, IonList } from '@ionic/react';
 import { RouteComponentProps } from 'react-router';
 import { PageContainer } from '../../components/page-container';
 
 export const Home: FC<HomeProps> = ({ history }) => {
-  const routes = [
-    { label: 'Inbox', route: '/inbox' },
-    { label: 'Settings', route: '/settings' },
-    { label: 'Analytics', route: '/analytics' },
-    { label: 'Storage', route: '/storage' },
-    { label: 'Sign in', route: '/sign-in' },
-    { label: 'Sign up', route: '/sign-up' },
-    { label: 'Forgot password', route: '/forgot-password' },
-    { label: 'Profile', route: '/profile' },
-    { label: 'Member card', route: '/member-card' },
-    { label: 'Reset password', route: '/reset-password/aaaaaa' },
-    { label: 'Account validation', route: '/account-validation/aaaaaa' },
-  ];
-
   return (
-    <PageContainer title="Home">
-      <IonList lines="full">
-        {routes.map(({ label, route }, index) => (
-          <IonItem key={index} button onClick={() => history.push(route)}>
-            <IonLabel className="ion-text-capitalize">{label}</IonLabel>
-          </IonItem>
-        ))}
-      </IonList>
+    <PageContainer noHeader>
+      <iframe title="Home" src={`https://demo.notificare.com/hybrid-app`} className="full-iframe" />
     </PageContainer>
   );
 };
