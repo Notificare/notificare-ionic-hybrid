e24f2a92606e57abbbb7ddae7cba178f7686c9fe
Helder Pinhal <helder@notifica.re>
2020-06-15 11:33:11 +0100

    Handle deep links

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -4,3 +4,17 @@ export function getPlatformName(): string | undefined {
   if (isPlatform('android')) return 'Android';
   if (isPlatform('ios')) return 'iOS';
 }
+
+export function trimSlashes(str: string): string {
+  let result = str;
+
+  if (result.startsWith('/')) {
+    result = result.slice(1);
+  }
+
+  if (result.endsWith('/')) {
+    result = result.slice(0, result.length - 1);
+  }
+
+  return result;
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,59 @@
-import React, { FC } from 'react';
+import URL from 'url';
+import React, { FC, useEffect } from 'react';
+import { AppUrlOpen, Plugins } from '@capacitor/core';
+import { Notificare } from '@ionic-native/notificare';
 import { RouteComponentProps } from 'react-router';
 import { PageContainer } from '../../components/page-container';
+import { showAlertDialog } from '../../lib/ui';
+import { trimSlashes } from '../../lib/utils';
 
 export const Home: FC<HomeProps> = ({ history }) => {
+  useEffect(() => {
+    // Deep links
+    Plugins.App.addListener('appUrlOpen', (data: AppUrlOpen) => handleDeepLink(data.url));
+
+    Notificare.on('scannableDetected', async (scannable) => {
+      if (scannable.notification) {
+        Notificare.presentScannable(scannable);
+      } else {
+        await showAlertDialog('Custom scannable found. The app is responsible for handling it.');
+      }
+    });
+
+    Notificare.on('scannableSessionInvalidatedWithError', async ({ error }) => showAlertDialog(error));
+
+    Notificare.on('urlOpened', (data) => handleDeepLink(data.url));
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const handleDeepLink = (urlStr: string) => {
+    console.log(`Received a deep link: ${urlStr}`);
+    const url = URL.parse(urlStr);
+
+    switch (url.path) {
+      case '/inbox':
+      case '/settings':
+      case '/regions':
+      case '/beacons':
+      case '/profile':
+      case '/analytics':
+      case '/storage':
+        history.push(trimSlashes(url.path));
+        break;
+      case '/membercard':
+        history.push('member-card');
+        break;
+      case '/signin':
+        history.push('sign-in');
+        break;
+      case '/signup':
+        history.push('sign-up');
+        break;
+      case '/scan':
+        Notificare.startScannableSession();
+        break;
+    }
+  };
+
   return (
     <PageContainer noHeader>
       <iframe title="Home" src={`https://demo.notificare.com/hybrid-app`} className="full-iframe" />
